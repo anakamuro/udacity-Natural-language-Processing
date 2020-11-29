e28d72c6d9fd0938f68ad9d8e7d4b6adba27af8c
Akihiko Nakamura <anakamuro@earthlink.net>
2020-11-29 16:37:48 -0500

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,31 +1,60 @@
-import { checkForName } from './nameChecker';
-
-const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit);
+const error = document.getElementById('error');
+const polarity = document.getElementById('polarity');
+const polConfidence = document.getElementById('pol-confidence');
+const subjectivity = document.getElementById('subjectivity');
+const subConfidence = document.getElementById('sub-confidence');
 
 function handleSubmit(event) {
     event.preventDefault()
 
-
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formUrl = document.getElementById('url').value;
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test', {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json"
-        }, 
-        body: JSON.stringify({formText})
-    })
 
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.polarity
-        console.log(res);
-    })
+    if (Client.validateUrl(formUrl)) {
+        
+        error.style.visibility = 'hidden';
+
+        polarity.innerHTML = "";
+        polConfidence.innerHTML = "";
+        subjectivity.innerHTML = "";
+        subConfidence.innerHTML = "";
+
+        postData(formUrl)
+            .then(function(data) {
+                updateUI(data);
+            })
+    }
+}
+
+const postData = async(url = '') => {
+
+    const response = await fetch('http://localhost:8080/test', {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ "url": url }),       
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData)
+        return newData
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+
+function updateUI(data) {
+    console.log(data)
+    polarity.innerHTML = `Polarity: ${data.polarity}`;
+    polConfidence.innerHTML = `Polarity Confidence: ${data.polarity_confidence}`;
+    subjectivity.innerHTML = `Subjectivity: ${data.subjectivity}`;
+    subConfidence.innerHTML = `Subjectivity Confidence: ${data.subjectivity_confidence}`;
 }
 
 export { handleSubmit }
