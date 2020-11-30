7a08421451ca4f6b11a06cabae777e6a50854257
Akihiko Nakamura <anakamuro@earthlink.net>
2020-11-30 10:41:41 -0500

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,26 @@
 var AYLIENTextAPI = require('aylien_textapi');
-var textapi = new AYLIENTextAPI({
-  application_id: "c678286a",
-  application_key: "66fba852bf4ea333e60b6f9cf99e83f8"
-});
 
-textapi.sentiment({
-    'text': 'John is a very good football player!'
-  }, function(error, response) {
-    if (error === null) {
-      console.log(response);
-    }
-  });
-  console.log('Your API key is ${process.env.API_KEY}')
+function inputValidateRequest(req, res, next) {
+    if(!req.body.text ) {
+        return res.status(400).json({
+           message: 'Invalid input'
+        })
+    } 
+    return next();
+}
 
-  const dotenv = require('dotenv');
-  dotenv.config();
+function registerPostHandler(req, res, next) {
+    var textapi = new aylien({
+        application_id: "c678286a",
+        application_key: "66fba852bf4ea333e60b6f9cf99e83f8"
+    });
+    textapi.sentiment({
+      'url': req.body.text
+    }, function(error, response) {
+        res.send(response)
+    }); 
+ 
+}
 
-  module.exports = textapi
+exports.inputValidateRequest = inputValidateRequest;
+exports.registerPostHandler = registerPostHandler;
