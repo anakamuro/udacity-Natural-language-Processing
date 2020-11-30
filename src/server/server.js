a7217391e03bba3c455b31db4c046431432186aa
Akihiko Nakamura <anakamuro@earthlink.net>
2020-11-30 14:35:48 -0500

    Update and rename handleRequest.js to server.js

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+var AYLIENTextAPI = require('aylien_textapi');
+var textapi = new AYLIENTextAPI({
+  application_id: "c678286a",
+  application_key: "66fba852bf4ea333e60b6f9cf99e83f8"
+});
+
+textapi.sentiment({
+    'text': 'John is a very good football player!'
+  }, function(error, response) {
+    if (error === null) {
+      console.log(response);
+    }
+  });
+  console.log('Your API key is ${process.env.API_KEY}')
+
+  const dotenv = require('dotenv');
+  dotenv.config();
+
+  module.exports = textapi
