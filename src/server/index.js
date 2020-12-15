e719a465134f97b540170a4f75df39352dc66775
Akihiko Nakamura <anakamuro@earthlink.net>
2020-12-15 14:38:08 -0500

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,6 @@
-var AYLIENTextAPI = require('aylien_textapi');
-var textapi = new AYLIENTextAPI({
-  application_id: "c678286a",
-  application_key: "66fba852bf4ea333e60b6f9cf99e83f8"
-});
-
-textapi.sentiment({
-  'text': 'John is a very good football player!'
-}, function(error, response) {
-  if (error === null) {
-    console.log(response);
-  }
-});
-console.log('Your API key is ${process.env.API_KEY}')
-
-const dotenv = require('dotenv');
-dotenv.config();
-
-module.exports = textapi
-
 var path = require('path')
 const express = require('express')
+const server = require('./server'}
 const mockAPIResponse = require('./mockAPI.js')
 const app = express()
 const bodyParser = require('body-parser');
@@ -37,7 +18,7 @@ app.get('/', function (req, res) {
 })
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8081!')
 });
 
 app.post('/article', (req, res) => {
