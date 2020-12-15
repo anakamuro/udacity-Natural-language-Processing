61ccef81fae01edd2839ebd10a7261c275633689
Akihiko Nakamura <anakamuro@earthlink.net>
2020-12-15 09:41:00 -0500

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -22,34 +22,36 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
 
 
 app.use(express.static('dist'))
 console.log(__dirname)
+
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('src/client/js/views/index.html'))
 })
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 });
 
-app.post('/article', async (req, res, next) =>{
-  try {
-    var data = textapi.sentiment({
-      //'text': 'John is a very good football player!'
-      'url': req.body.text
-    }, function(error, response) {
-      if (error === null) {
-        console.log(response);
-        res.send(response);
-      }
-    });
-    //res.send(mockAPIResponse)
-  } catch(error) {
-    // Passes errors into the handler
-    return next(error)
-  }
-  //res.send(returnVal);
+app.post('/article', (req, res) => {
+  textapi.sentiment({ url: req.body.url }, (error, result) => {
+        if(error) {
+          console.log('Error during Aylien request')
+          res.send();
+          return;
+        }
+
+        console.log('Got Aylien result')
+        
+        res.send(result);
+      })
 })
+
+module.exports = app;
