b8582efded2260059e61f143feb3c984beebb2c0
Akihiko Nakamura <anakamuro@earthlink.net>
2020-12-15 09:30:36 -0500

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,31 +1,67 @@
+const error = document.getElementById('error');
+const polarity = document.getElementById('polarity');
+const polConfidence = document.getElementById('pol-confidence');
+const subjectivity = document.getElementById('subjectivity');
+const subConfidence = document.getElementById('sub-confidence');
+
+
+
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkerUrl(formText)
+    let formUrl = document.getElementById('url').value;
+    Client.checkerUrl(formUrl)
 
-    console.log("::: Form has been Submitted :::")
-     fetch('./article')
-     .then(res => res.json())
-     .then(function(res) {
-         document.getElementById('results').innerHTML = res.message
-    })
-    let reqBody = {
-        text: formText
-    };
+    console.log("::: Form Submitted :::")
 
-    fetch('/article', {
+    if (Client.checkerUrl(formUrl)) {
+        // Hide error message
+        error.style.visibility = 'hidden';
+
+        // Clear previous form results
+        polarity.innerHTML = "";
+        polConfidence.innerHTML = "";
+        subjectivity.innerHTML = "";
+        subConfidence.innerHTML = "";
+
+        // Call postData passing in the entered url
+        postData(formUrl)
+            .then(function(data) {
+                // Update Webpage with results
+                updateUI(data);
+            })
+    }
+}
+// Async POST
+const postData = async(url = '') => {
+
+    const response = await fetch('http://localhost:8080/article', {
         method: 'POST',
-        body: JSON.stringify(reqBody),
-        headers: {"Content-Type": "application/json"}
-    })
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.polarity
-        console.log(res);
-        alert(dataText);
-    })
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ "url": url }), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData)
+        return newData
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+
+function updateUI(data) {
+    console.log(data)
+    polarity.innerHTML = `Polarity: ${data.polarity}`;
+    polConfidence.innerHTML = `Polarity Confidence: ${data.polarity_confidence}`;
+    subjectivity.innerHTML = `Subjectivity: ${data.subjectivity}`;
+    subConfidence.innerHTML = `Subjectivity Confidence: ${data.subjectivity_confidence}`;
 }
 
 export { handleSubmit }
